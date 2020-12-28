92583c8d5c1188c8774c85c99d7829e10eb3dd02
hbrandao <brandao.t.henrique@gmail.com>
2020-12-28 12:31:40 -0300

    checkpoint

--- notebooks/utils/utils.py
+++ notebooks/utils/utils.py
@@ -1,9 +1,11 @@
-import pandas
+import pandas as pd
+
 
 def _value_counts(df):
     """
     Generalizes pd.value_counts() to all columns
     """
+    print('>> Value counts:\n')
     for c in df.columns:
         print(f'# {c}:\n{df[c].value_counts()}\n\n')
 
@@ -12,6 +14,7 @@ def _describe(df):
     """
     Generalizes pd.describe() to all columns
     """
+    print('>> Describe columns:\n')
     for c in df.columns:
         print(f'# {c}:\n{df[c].describe()}\n\n')
 
@@ -20,7 +23,10 @@ def _isnull(df):
     """
     Generalizes pd.isnull() to all columns
     """
+    print('>> Null registers:\n')
     for c in df.columns:
-        cnt = df[pandas.isnull(df[c])].shape[0]
-        if cnt != 0:
+        cnt = df[pd.isnull(df[c])].shape[0]
+        if cnt == 0:
+            print(f'# {c}: 0 null rows')
+        else:
             print(f'# {c}: {cnt} rows')
